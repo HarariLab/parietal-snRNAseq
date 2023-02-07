2ef1dcdfd14ad55894eb3d81b4cd77ac0b66ec78
Logan Brase <32940699+braselog@users.noreply.github.com>
2023-02-07 14:45:35 -0600

    Add scripts used for differential expression

---
+++ Nebula_DE_scripts/by_pheno/shell/run_sADvsCO_neuron_Nebula.sh
@@ -0,0 +1,6 @@
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron all
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron excit
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron inhib
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron 0
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron 1
+Rscript /home/bnovotny/single_nuclei/neuron/nebula_de/DE_by_status/sADvsCO/DE_Nebula_sAD_Neuron.R neuron 2
