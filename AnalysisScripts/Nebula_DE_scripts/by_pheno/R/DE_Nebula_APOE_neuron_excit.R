e302b8e3368f9a395f9034a0c0bc650dcea7cc21
Logan Brase <32940699+braselog@users.noreply.github.com>
2023-02-09 14:03:02 -0600

    Add nebula scripts
    scripts for differential expression analyses

---
+++ AnalysisScripts/Nebula_DE_scripts/by_pheno/R/DE_Nebula_APOE_neuron_excit.R
@@ -0,0 +1,108 @@
+#!/bin/env Rscript
+
+library(nebula)
+library(Seurat)
+
+pvalue.extreme <- function(z) {
+  log.pvalue <- log(2) + pnorm(abs(z), lower.tail = FALSE, log.p = TRUE)
+  log10.pvalue <- log.pvalue/log(10) ## from natural log to log10
+  mantissa <- 10^(log10.pvalue %% 1)
+  exponent <- log10.pvalue %/% 1
+  ## or return(c(mantissa,exponent))
+  #return(sprintf("p value is %1.2f times 10^(%d)",mantissa,exponent))
+  return(sprintf("%1.2fE%d",mantissa,exponent))
+}
+
+BH.extreme <- function(Zs) {
+  len = length(Zs)
+  
+  log_pval <- log(2) + pnorm(abs(Zs), lower.tail = FALSE, log.p = TRUE) + log(len) - log(1:len)
+  log10_pval <- log_pval/log(10)
+  mantissa <- 10^(log10_pval %% 1)
+  exponent <- log10_pval %/% 1
+  BHs <- sprintf("%1.2fE%d",mantissa,exponent)
+  
+  for(i in 1:length(BHs)){ # for each BH calculated so far
+    change = which(as.numeric(BHs[i]) < as.numeric(BHs[1:(i-1)])) # find if previous BH is larger 
+    if(length(change) > 0){  BHs[change] = BHs[i]  } # If a previous BH is larger, replace it with the current BH
+  }
+  return(BHs)
+}
+
+# Could also specify statuses to compare
+args = commandArgs(trailingOnly=TRUE)
+celltype = args[1]
+subclust = args[2]
+
+print(paste("Running", celltype, subclust))
+t1 <- Sys.time()
+
+m = readRDS(paste0('/home/brasel/SingleCellProjects/dataObjects/neuron.rds'))
+excit = c(0,1,2,6)
+inhib = c(3,4,5)
+
+# Filter excitatory or inhibitory if needed
+if(subclust == "excit"){ 
+  m = subset(m,subset=SCT_snn_res.0.1 %in% excit)
+}else if (subclust == "inhib") {
+  m = subset(m,subset=SCT_snn_res.0.1 %in% inhib)
+}
+
+m@meta.data$Status = toupper(m@meta.data$Status)
+
+# Could specify status in arguments
+m = subset(m,subset= nTREM2 != "TREM2" & Status == "NEURO_AD")
+
+# apoe_table <- data.frame(as.data.frame.matrix(table(m@meta.data$Sample_ID, m@meta.data$nAPOE)))
+
+m = subset(m,subset=Sample_ID %in% names(table(m@meta.data$Sample_ID)[table(m@meta.data$Sample_ID) > 60]))
+m@meta.data$Gender = as.factor(m@meta.data$Gender)
+m@meta.data$AOD = as.numeric(m@meta.data$AOD)
+m@meta.data$nAPOE = factor(m@meta.data$nAPOE, levels = c("33", "34"))
+
+# Subset to cluster we want
+clusterSet = as.numeric(as.character(Idents(m)))
+
+
+if (!subclust %in% c('excit', 'inhib', 'all')){
+  # 1 vs 1
+  clusterSet[clusterSet != subclust] = 99
+}
+
+m@meta.data$cluster = clusterSet
+
+m = subset(m,subset=cluster != 99)
+
+
+pred = model.matrix(~nAPOE + AOD,data=m@meta.data)
+
+re_ln = nebula(GetAssayData(m,slot='counts'),m@meta.data$Sample_ID,pred=pred,method='LN',model='NBLMM')
+
+
+tmp = data.frame(gene=re_ln$summary$gene, logFC_nAPOE34=re_ln$summary$logFC_nAPOE34)
+tmp$z.score = re_ln$summary$logFC_nAPOE34/re_ln$summary$se_nAPOE34
+tmp$p.value = pvalue.extreme(tmp$z.score)
+tmp <- tmp[order(-abs(tmp$z.score)),]
+tmp$BH = BH.extreme(tmp$z.score)
+merged = merge(tmp,re_ln$summary,by=c('gene', "logFC_nAPOE34"))
+rownames(merged) = merged$gene
+merged = merged[order(abs(merged$z.score),decreasing=T),c(2:14,1)]
+
+
+dir <- paste0('/home/bnovotny/single_nuclei/nebula_67/', celltype, '/nebula_de/DE_by_status/APOE/')
+mainfilename <- paste('nebula_compareAPOE4_fixed', celltype, subclust, sep='_')
+
+
+write.table(merged,file=paste0(dir, mainfilename,'.txt'),quote=F,sep='\t')
+write.table(merged[as.numeric(merged$BH) < 0.05,],file=paste0(dir, 'bh_',mainfilename,'.txt'),quote=F,sep='\t')
+
+t2 <- Sys.time()
+print(t2-t1)
+print(paste("Finished", celltype, subclust))
+print("---")
+
+
+
+
+
+
